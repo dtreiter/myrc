6d9ba9d99033d9ecfd22ed97a7c29902ac63000b
Daniel Reiter <daniel.reiter@fa-danielreiter.gwp.corp.flightaware.com>
2014-09-18 12:42:38 -0500

    Added mouse support in emacs
    Previously the mouse support was not very good and didn't support scrolling.
    Now you can use the mouse to scroll.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,10 +12,18 @@
 (setq linum-format "%d ")
 
 ;;; enable mouse
-(require 'mouse)
-(xterm-mouse-mode t)
-(defun track-mouse (e))
-(setq mouse-sel-mode t)
+(unless window-system
+  (require 'mouse)
+  (xterm-mouse-mode t)
+  (global-set-key [mouse-4] '(lambda ()
+                              (interactive)
+                              (scroll-down 1)))
+  (global-set-key [mouse-5] '(lambda ()
+                              (interactive)
+                              (scroll-up 1)))
+  (defun track-mouse (e))
+  (setq mouse-sel-mode t)
+)
 
 ;;; use elpa
 (require 'package)
