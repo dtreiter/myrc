18705651e093b28edf772ef6be0f9ffe8aa32784
Daniel Reiter <semibmx@gmail.com>
2015-12-14 15:24:10 -0600

    Make install script
    The install script will link anything ending in .symlink to the
    home directory.

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# The source directory and target directories.
+SOURCE="$PWD"
+TARGET="$(echo $HOME)/test"
+
+FILES="$(find . -name '*.symlink')"
+for FILE in $FILES; do
+    SOURCE_FILE=$(basename "$FILE")
+    TARGET_FILE="${SOURCE_FILE%.*}"
+    echo "Linking $SOURCE/$SOURCE_FILE to $TARGET/$TARGET_FILE"
+    ln -s "$SOURCE/$SOURCE_FILE" "$TARGET/$TARGET_FILE"
+done
